5df7dd7934477810b18aded819a1f819540bc075
lolieatapple <lolieatapple@gmail.com>
2024-11-08 16:35:20 +0800

    default logs from 10 to 25

--- src/api/controllers/activityLogsController.js
+++ src/api/controllers/activityLogsController.js
@@ -4,7 +4,7 @@ exports.getLogs = async (ctx) => {
   const { plyrId } = ctx.params;
   let { offset, limit } = ctx.query;
   offset = parseInt(offset) || 0;
-  limit = parseInt(limit) || 10;
+  limit = parseInt(limit) || 25;
   const logs = await ActivityLog.find({ plyrId }).sort({ timestamp: -1 }).skip(offset).limit(limit);
   ctx.body = logs;
 }
