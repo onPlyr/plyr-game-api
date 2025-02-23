e14024c8df6a7a2e445a4cb1ab17ddf8fafe6c44
lolieatapple <lolieatapple@gmail.com>
2025-02-23 22:34:31 +0800

    fix: auth data type

--- src/models/auth.js
+++ src/models/auth.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const authSchema = new mongoose.Schema({
   uid: { type: String, required: true, unique: true },
-  data: { type: String, required: true, unique: true },
+  data: { type: Object, required: true, unique: true },
   createdAt: { type: Date, default: Date.now }
 });
 
