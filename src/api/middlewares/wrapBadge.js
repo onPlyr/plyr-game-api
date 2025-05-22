dd4ce01f7984254ac6305dd337c406058cfc00e6
lolieatapple <lolieatapple@gmail.com>
2025-05-22 21:16:43 +0800

    feat: support chainId validation from both request body and query params in badge middleware

--- src/api/middlewares/wrapBadge.js
+++ src/api/middlewares/wrapBadge.js
@@ -2,7 +2,8 @@
 const wrapBadge = async (ctx, next) => {
     // add isBadge = true to body
     const { chainId } = ctx.request.body;
-    if (![16180, 62831].includes(chainId)) {
+    const { _chainId } = ctx.query;
+    if (![16180, 62831].includes(chainId || _chainId)) {
         ctx.status = 400;
         ctx.body = { error: 'Only plyr and plyrTestnet support badge' };
         return;
