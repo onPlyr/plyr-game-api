f853bb4a39cacfc520d6a525886a6f94f231033d
lolieatapple <lolieatapple@gmail.com>
2025-05-23 22:32:14 +0800

    fix: update badge to nft field mapping in request body and add chainId to query params

--- src/api/middlewares/wrapBadge.js
+++ src/api/middlewares/wrapBadge.js
@@ -3,16 +3,17 @@ const { chain } = require('../../config');
 const wrapBadge = async (ctx, next) => {
     // add isBadge = true to body
     ctx.request.body.chainId = chain.chain.id;
+    ctx.query.chainId = chain.chain.id.toString();
     console.log('wrap Badge chainId', chain.chain.id, chain.chain);
     ctx.request.body.isBadge = true;
     ctx.query.isBadge = true;
 
-    if (ctx.body?.badges) {
-        ctx.body.nfts = ctx.body.badges;
+    if (ctx.request.body?.badges) {
+        ctx.request.body.nfts = ctx.request.body.badges;
     }
 
-    if (ctx.body?.badge) {
-        ctx.body.nft = ctx.body.badge;
+    if (ctx.request.body?.badge) {
+        ctx.request.body.nft = ctx.request.body.badge;
     }
 
     if (ctx.query?.badge) {
